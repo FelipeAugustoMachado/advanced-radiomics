37e6032d2a1fe4a9cd8f7f1f2baffb9ab21e98ad
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-11-10 01:44:16 +0000

    Update docs after building Travis build 6 of FelipeAugustoMachado/advanced-radiomics
    The docs were built from the branch 'master' against the commit
    27c7f87776eecb9f4cbbdefb032bd0b5589eaa8c.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/FelipeAugustoMachado/advanced-radiomics/jobs/609820816.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.post1+g404cf58',
+    VERSION: '0.1.0.post4+g27c7f87',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
